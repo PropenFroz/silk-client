1b3d7c137b9d86296341ebc904a4f15f1f5c41ba
ririnzakiyah02 <ririnzakiyah02@Gmail.com>
2024-03-09 22:12:42 +0700

    feat: handling issue start date > end date

--- src/pages/laporanKeuanganBuku.js
+++ src/pages/laporanKeuanganBuku.js
@@ -12,18 +12,22 @@ export default function LaporanKeuanganBuku() {
   const [endDate, setEndDate] = useState(null);
   const [transactions, setTransactions] = useState([]);
 
-  const handleExport = () => {
-    if (!startDate || !endDate) {
-        alert("Mohon isi kedua tanggal terlebih dahulu.");
-        return;
-    } else {
-        const formattedStartDate = new Date(startDate.getTime() - (startDate.getTimezoneOffset() * 60000)).toISOString().split('T')[0];
-        const formattedEndDate = new Date(endDate.getTime() - (endDate.getTimezoneOffset() * 60000)).toISOString().split('T')[0];
+    const handleExport = () => {
+      if (!startDate || !endDate) {
+          alert("Mohon isi kedua tanggal terlebih dahulu.");
+          return;
+      } else if (startDate > endDate) {
+          alert("Mohon pilih tanggal awal yang lebih kecil dari tanggal akhir.");
+          return;
+      }
+      else {
+          const formattedStartDate = new Date(startDate.getTime() - (startDate.getTimezoneOffset() * 60000)).toISOString().split('T')[0];
+          const formattedEndDate = new Date(endDate.getTime() - (endDate.getTimezoneOffset() * 60000)).toISOString().split('T')[0];
 
-        const url = `http://localhost:8080/api/entry-transaksi-buku/laporan?startDate=${formattedStartDate}&endDate=${formattedEndDate}`;
-        window.open(url, '_blank');
-    }
-};
+          const url = `http://localhost:8080/api/entry-transaksi-buku/laporan?startDate=${formattedStartDate}&endDate=${formattedEndDate}`;
+          window.open(url, '_blank');
+      }
+    };
 
 
   const handleView = async () => {
