d992fef4f4ce33ac42ab50321c143db6148815da
ririnzakiyah02 <ririnzakiyah02@Gmail.com>
2024-03-12 23:19:14 +0700

    ubah password

--- src/components/modalUbahPassword.js
+++ src/components/modalUbahPassword.js
@@ -13,10 +13,12 @@ function ModalUbahPassword({ formData, show, onHide, onSuccess }) {
     <Modal show={show} onHide={onHide}>
       <Modal.Title>Change Password</Modal.Title>
       <Modal.Body>
-        <div className="label">Nama</div>
-        <div className="password">Abdillah Katab Panggabean</div>
-        <div className="label">Role</div>
-        <div className="password">Karyawan</div>
+        <div className="label">Current Password</div>
+        <div className="password">xxxxxxxxxxx</div>
+        <div className="label">New Password</div>
+        <div className="password">xxxxxxxxxx</div>
+        <div className="label">Confirm Password</div>
+        <div className="password">xxxxxxxxxx</div>
       </Modal.Body>
       <Modal.Footer>
         <button className="btn-submit" onClick={handleSubmit}>Submit</button>
