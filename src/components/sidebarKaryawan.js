9410ff28f9e1c8a1f9c001a2413b7b532134d196
rafliwasis <rafli.wasis@ui.ac.id>
2024-05-12 14:31:40 +0700

    push Wasis

--- src/components/sidebarKaryawan.js
+++ src/components/sidebarKaryawan.js
@@ -1,18 +1,10 @@
 import React from "react";
-import { 
-  CDBSidebar,
-  CDBSidebarContent,
-  CDBSidebarFooter,
-  CDBSidebarHeader,
-  CDBSidebarMenu,
-  CDBSidebarMenuItem } from "cdbreact";
+import { CDBSidebar, CDBSidebarContent, CDBSidebarFooter, CDBSidebarHeader, CDBSidebarMenu, CDBSidebarMenuItem } from "cdbreact";
 import { NavLink, useHistory } from "react-router-dom";
-import "../styles/sidebar.css"; 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
-import { useAuth } from './auth/context/AuthContext';
-
-
+import "../styles/sidebar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import { useAuth } from "./auth/context/AuthContext";
 
 const SideBarKaryawan = () => {
   const history = useHistory(); // Initialize history
@@ -20,83 +12,101 @@ const SideBarKaryawan = () => {
 
   const handleLogout = () => {
     userLogout(); // Call userLogout function from useAuth hook
-    history.push('/login'); // Redirect to login page after logout
+    history.push("/login"); // Redirect to login page after logout
   };
 
   return (
     <div className={`app`}>
-      <CDBSidebar
-        textColor="#1C1C1C"
-        backgroundColor="#FFFFFF"
-      >
-        <CDBSidebarHeader
-          prefix={
-            <i className="fa fa-bars fa-large"></i>
-          }
-        >
-          <a href="/homepage-karyawan" className="text-decoration-none style" style={{color:"inherit"}}>
-          <span className="brand-text">Purwacaraka</span>
+      <CDBSidebar textColor="#1C1C1C" backgroundColor="#FFFFFF">
+        <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
+          <a href="/homepage-karyawan" className="text-decoration-none style" style={{ color: "inherit" }}>
+            <span className="brand-text">Purwacaraka</span>
           </a>
         </CDBSidebarHeader>
 
         <CDBSidebarContent className="sidebar-content">
-        <div className="sidebar-section-label">Input Data</div>
+          <div className="sidebar-section-label">Input Data</div>
           <CDBSidebarMenu>
             <NavLink exact to="/entry-transaksi-siswa" activeClassName="activeClicked">
-                <CDBSidebarMenuItem icon="fa-solid fa-user-graduate" className="sidebar-menu-item">Transaksi Pendaftaran Siswa</CDBSidebarMenuItem>
+              <CDBSidebarMenuItem icon="fa-solid fa-user-graduate" className="sidebar-menu-item">
+                Transaksi Pendaftaran Siswa
+              </CDBSidebarMenuItem>
             </NavLink>
             <NavLink exact to="/entry-transaksi-siswa-kursus" activeClassName="activeClicked">
-                <CDBSidebarMenuItem icon="fa-solid fa-user-graduate" className="sidebar-menu-item">Transaksi Kursus Siswa</CDBSidebarMenuItem>
+              <CDBSidebarMenuItem icon="fa-solid fa-user-graduate" className="sidebar-menu-item">
+                Transaksi Kursus Siswa
+              </CDBSidebarMenuItem>
             </NavLink>
             <NavLink exact to="/entry-transaksi-siswa-lainnya" activeClassName="activeClicked">
-                <CDBSidebarMenuItem icon="fa-solid fa-user-graduate" className="sidebar-menu-item">Transaksi Lainnya Siswa</CDBSidebarMenuItem>
+              <CDBSidebarMenuItem icon="fa-solid fa-user-graduate" className="sidebar-menu-item">
+                Transaksi Lainnya Siswa
+              </CDBSidebarMenuItem>
             </NavLink>
             <NavLink exact to="/entry-pembelian-buku" activeClassName="activeClicked">
-                <CDBSidebarMenuItem icon="fa-solid fa-book" className="sidebar-menu-item">Pembelian Buku</CDBSidebarMenuItem>
+              <CDBSidebarMenuItem icon="fa-solid fa-book" className="sidebar-menu-item">
+                Transaksi Buku
+              </CDBSidebarMenuItem>
             </NavLink>
             <NavLink exact to="/entry-gaji-guru" activeClassName="activeClicked">
-                <CDBSidebarMenuItem icon="fa-solid fa-wallet" className="sidebar-menu-item">Gaji Guru</CDBSidebarMenuItem>
+              <CDBSidebarMenuItem icon="fa-solid fa-wallet" className="sidebar-menu-item">
+                Gaji Guru
+              </CDBSidebarMenuItem>
             </NavLink>
-           
+
             <hr className="sidebar-divider" />
             <div className="sidebar-section-label">Laporan</div>
             <NavLink exact to="/laporan-transaksi-siswa" activeClassName="activeClicked">
-                <CDBSidebarMenuItem icon="fa-regular fa-clipboard" className="sidebar-menu-item">Transaksi Siswa</CDBSidebarMenuItem>
+              <CDBSidebarMenuItem icon="fa-regular fa-clipboard" className="sidebar-menu-item">
+                Transaksi Siswa
+              </CDBSidebarMenuItem>
             </NavLink>
             <NavLink exact to="/laporan-iuran-siswa" activeClassName="activeClicked">
-                <CDBSidebarMenuItem icon="fa-regular fa-clipboard" className="sidebar-menu-item">Iuran</CDBSidebarMenuItem>
+              <CDBSidebarMenuItem icon="fa-regular fa-clipboard" className="sidebar-menu-item">
+                Iuran
+              </CDBSidebarMenuItem>
             </NavLink>
             <NavLink exact to="/laporan-pembayaran-kursus" activeClassName="activeClicked">
-                <CDBSidebarMenuItem icon="fa-regular fa-clipboard" className="sidebar-menu-item">Pembayaran Kursus</CDBSidebarMenuItem>
+              <CDBSidebarMenuItem icon="fa-regular fa-clipboard" className="sidebar-menu-item">
+                Pembayaran Kursus
+              </CDBSidebarMenuItem>
             </NavLink>
             <NavLink exact to="/laporan-keuangan-buku" activeClassName="activeClicked">
-                <CDBSidebarMenuItem icon="fa-regular fa-clipboard" className="sidebar-menu-item">Keuangan & Stok Buku</CDBSidebarMenuItem>
+              <CDBSidebarMenuItem icon="fa-regular fa-clipboard" className="sidebar-menu-item">
+                Keuangan & Stok Buku
+              </CDBSidebarMenuItem>
             </NavLink>
             <NavLink exact to="/laporan-gaji-guru" activeClassName="activeClicked">
-                <CDBSidebarMenuItem icon="fa-regular fa-clipboard" className="sidebar-menu-item">Gaji Guru</CDBSidebarMenuItem>
+              <CDBSidebarMenuItem icon="fa-regular fa-clipboard" className="sidebar-menu-item">
+                Gaji Guru
+              </CDBSidebarMenuItem>
             </NavLink>
             <NavLink exact to="/laporan-data-siswa" activeClassName="activeClicked">
-                <CDBSidebarMenuItem icon="fa-regular fa-clipboard" className="sidebar-menu-item">Data Siswa</CDBSidebarMenuItem>
+              <CDBSidebarMenuItem icon="fa-regular fa-clipboard" className="sidebar-menu-item">
+                Data Siswa
+              </CDBSidebarMenuItem>
             </NavLink>
             <NavLink exact to="/laporan-daftar-siswa" activeClassName="activeClicked">
-                <CDBSidebarMenuItem icon="fa-regular fa-clipboard" className="sidebar-menu-item">Daftar Siswa</CDBSidebarMenuItem>
+              <CDBSidebarMenuItem icon="fa-regular fa-clipboard" className="sidebar-menu-item">
+                Daftar Siswa
+              </CDBSidebarMenuItem>
             </NavLink>
             <NavLink exact to="/laporan-buku" activeClassName="activeClicked">
-                <CDBSidebarMenuItem icon="fa-regular fa-clipboard" className="sidebar-menu-item">Buku</CDBSidebarMenuItem>
+              <CDBSidebarMenuItem icon="fa-regular fa-clipboard" className="sidebar-menu-item">
+                Buku
+              </CDBSidebarMenuItem>
             </NavLink>
           </CDBSidebarMenu>
-          <CDBSidebarMenu>
-          </CDBSidebarMenu>
+          <CDBSidebarMenu></CDBSidebarMenu>
         </CDBSidebarContent>
 
         <CDBSidebarFooter style={{ textAlign: "center" }}>
-          <div className="sidebar-btn-wrapper" style={{ padding: "20px 5px"}}>
+          <div className="sidebar-btn-wrapper" style={{ padding: "20px 5px" }}>
             <NavLink to="/profile" className="text-decoration-none" style={{ color: "inherit" }}>
               <FontAwesomeIcon icon={faUser} /> Profile Saya
             </NavLink>
           </div>
 
-          <div className="sidebar-btn-wrapper" style={{ padding: "20px 5px"}}>
+          <div className="sidebar-btn-wrapper" style={{ padding: "20px 5px" }}>
             <button onClick={handleLogout} className="text-decoration-none" style={{ color: "inherit", border: "none", background: "none", cursor: "pointer" }}>
               <FontAwesomeIcon icon={faSignOutAlt} /> Logout
             </button>
@@ -105,6 +115,6 @@ const SideBarKaryawan = () => {
       </CDBSidebar>
     </div>
   );
-}
+};
 
 export default SideBarKaryawan;
